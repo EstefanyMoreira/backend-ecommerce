2ce16ebed468ab0c2e91b1141ca282c8d3b5a1d6
Sofia Osano Corbo <sofia-199906@hotmail.com>
2024-11-23 20:49:32 -0300

    login

--- app.js
+++ app.js
@@ -2,16 +2,23 @@
   const cors = require('cors'); // Importar el paquete cors
   const fs = require('fs'); //https://www.w3schools.com/nodejs/nodejs_filesystem.asp, permite interactuar con el sistema de archivos
   const path = require('path'); //https://www.w3schools.com/nodejs/ref_path.asp, trabajar con directorios y rutas de archivos
+  const jwt = require('jsonwebtoken'); // Importar jsonwebtoken
+
+  const KEY = "clave" // Llave para firmar los tokens JWT
   const app = express(); 
   const port = 3000; 
   
   app.get("/", (req, res) => {
-  
     res.send("<h1>Bienvenido a mi servidor</h1>");
   });
 
+  // Habilitar CORS para permitir solicitudes desde otros dominios
   app.use(cors());
-  // Middleware para servir archivos estáticos en la carpeta "data.json" 
+
+  // Middleware para analizar cuerpos de solicitudes JSON
+  app.use(express.json());
+
+  // Middleware para servir archivos estáticos en la carpeta "data" 
   app.use('/data', express.static(path.join(__dirname, 'data')));
 
   // Ruta para listar el contenido de una subcarpeta 
@@ -43,6 +50,19 @@
         }  
         res.json(JSON.parse(data)); 
     }); 
+});
+
+// Ruta para login: valida credenciales y genera un token JWT si son correctas
+app.post('/login', (req, res) => {
+  const {username, password} = req.body; // Extraer username y password del cuerpo de la solicitud
+
+  // Validar usuario y contraseña
+  if (username === "admin" && password === "admin") {
+    const token = jwt.sign({username}, KEY); // Genera un token JWT con la llave KEY
+    res.status(200).json({token}); // Enviar el token en la respuesta
+  } else {
+    res.status(401).json({message: "usuario y/o contraseña incorrecto"});
+  }
 });
 
 app.listen(port, () => { 
